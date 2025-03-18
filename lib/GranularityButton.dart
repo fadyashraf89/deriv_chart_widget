38619d95c2f8552dda8a92970ad7d9210bca9ec9
fadyashraf89 <fadyashraf89@gmail.com>
2025-03-18 13:14:18 +0200

    first commit

--- lib/GranularityButton.dart
+++ lib/GranularityButton.dart
@@ -4,12 +4,14 @@ class GranularityButton extends StatefulWidget {
   final Function(int) onGranularityChanged;
   final List<int> granularities;
   final ButtonStyle? buttonStyle;
+  final TextStyle? textStyle;
 
   const GranularityButton({
     super.key,
     required this.onGranularityChanged,
     required this.granularities,
     this.buttonStyle,
+    this.textStyle
   });
 
   @override
@@ -28,60 +30,58 @@ class _GranularityButtonState extends State<GranularityButton> {
 
   String getGranularityLabel(int granularity) {
     switch (granularity) {
-      case 0:
-        return '1 Tick';
       case 5:
-        return '5 Seconds';
+        return '5s';
       case 10:
-        return '10 Seconds';
+        return '10s';
       case 15:
-        return '15 Seconds';
+        return '15s';
       case 30:
-        return '30 Seconds';
+        return '30s';
       case 60:
-        return '1 Minute';
+        return '1m';
       case 120:
-        return '2 Minutes';
+        return '2m';
       case 180:
-        return '3 Minutes';
+        return '3m';
       case 300:
-        return '5 Minutes';
+        return '5m';
       case 900:
-        return '15 Minutes';
+        return '15m';
       case 1800:
-        return '30 Minutes';
+        return '30m';
       case 3600:
-        return '1 Hour';
+        return '1H';
       case 7200:
-        return '2 Hours';
+        return '2H';
       case 14400:
-        return '4 Hours';
+        return '4H';
       case 21600:
-        return '6 Hours';
+        return '6H';
       case 43200:
-        return '12 Hours';
+        return '12H';
       case 86400:
-        return '1 Day';
+        return '1D';
       case 604800:
-        return '1 Week';
+        return '1W';
       case 2592000:
-        return '1 Month';
+        return '1M';
       case 31536000:
-        return '1 Year';
+        return '1Y';
       case 63072000:
-        return '2 Years';
+        return '2Y';
       case 94608000:
-        return '3 Years';
+        return '3Y';
       case 157680000:
-        return '5 Years';
+        return '5Y';
       case 315360000:
-        return '10 Years';
+        return '10Y';
       case 630720000:
-        return '20 Years';
+        return '20Y';
       case 1576800000:
-        return '50 Years';
+        return '50Y';
       case 3153600000:
-        return '100 Years';
+        return '100Y';
       default:
         return 'Unknown Granularity';
     }
@@ -105,7 +105,10 @@ class _GranularityButtonState extends State<GranularityButton> {
             });
             widget.onGranularityChanged(granularity);
           },
-          child: Text(getGranularityLabel(granularity)),
+          child: Text(getGranularityLabel(granularity), style: widget.textStyle ?? TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.bold
+          ),),
         );
       }).toList(),
     );
